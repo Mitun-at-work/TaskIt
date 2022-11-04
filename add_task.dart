a1773395023bce0ec0ccf5627f9adc9eeada113c
Mitun Suresh <116903938+Mitun-at-work@users.noreply.github.com>
2022-11-05 00:43:36 +0530

    Updated Working

--- add_task.dart
+++ add_task.dart
@@ -5,8 +5,61 @@ class AddTask extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const [],
+    return const Scaffold(
+      body: AddTaskHome(),
+    );
+  }
+}
+
+class AddTaskHome extends StatelessWidget {
+  const AddTaskHome({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: const Icon(Icons.done_outline_rounded),
+      ),
+      body: Column(
+        children: <Widget>[
+          Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(25),
+                margin: const EdgeInsets.all(25),
+                decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(
+              vertical: 50,
+            ),
+            padding: const EdgeInsets.all(25),
+            child: TextField(
+              onSubmitted: ((value) {
+                print("object");
+              }),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
